341ae2d02ae673035d6bc15db7fb91847a2569a6
djrlj694 <robert.l.jones@synthelytics.com>
2018-12-31 21:11:51 -0500

    Initial project setup

---
+++ NetworkKit/NetworkKit.h
@@ -0,0 +1,19 @@
+//
+//  NetworkKit.h
+//  NetworkKit
+//
+//  Created by Robert L. Jones on 12/31/18.
+//  Copyright © 2018 Synthelytics LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkKit.
+FOUNDATION_EXPORT double NetworkKitVersionNumber;
+
+//! Project version string for NetworkKit.
+FOUNDATION_EXPORT const unsigned char NetworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkKit/PublicHeader.h>
+
+
